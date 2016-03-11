6a9b26d03bd9f88127ee287da8e3e4f25ad704d8
sculove <sculove@gmail.com>
2016-03-12 02:03:44 +0900

    init

---
+++ plus.py
@@ -0,0 +1,55 @@
+#-*-coding: utf-8 -*-
+import logging
+from PyQt4.QtWebKit import QWebView, QWebPage, QWebInspector, QWebSettings
+from PyQt4.QtGui import QShortcut
+from PyQt4.QtCore import Qt
+
+class WebViewPlus(QWebView):
+    """
+    WebView 커스터마이징
+     - inspector 추가
+     - jsconsole 로그 추가
+    """
+    def __init__(self):
+        super().__init__()
+        _page = WebPagePlus()
+        self.setPage(_page)
+        self._setupInspector()
+
+    def _setupInspector(self):
+        """
+        F12키를 누르면 "개발자 도구"가 노출됨
+        """
+        self.settings().setAttribute(QWebSettings.DeveloperExtrasEnabled, True)
+        self.webInspector = QWebInspector(self)
+        self.webInspector.setPage(self.page())
+
+        shortcut = QShortcut(self)
+        shortcut.setKey(Qt.Key_F12)
+        shortcut.activated.connect(self._toggleInspector)
+        self.webInspector.setVisible(False)
+
+
+    def _toggleInspector(self):
+        """
+        F12키를 다시 누르면 "개발자 도구"가 사라짐
+        """
+        self.webInspector.setVisible(not self.webInspector.isVisible())
+
+
+
+class WebPagePlus(QWebPage):
+    """
+    javascript 콘솔 메시지를 python logger에 출력
+    http://pyqt.sourceforge.net/Docs/PyQt4/qwebpage.html
+    """
+    def __init__(self, logger=None):
+        super().__init__()
+        if not logger:
+            logger = logging
+        self.logger = logger
+
+
+    def javaScriptConsoleMessage(self, msg, lineNumber, sourceID):
+        self.logger.warning("console(%s:%d): %s" % (sourceID, lineNumber, msg))
+
---
+++ wnd.py
@@ -0,0 +1,64 @@
+#-*-coding: utf-8 -*-
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import QWidget, QSplitter, QVBoxLayout, QApplication
+from kiwoom.core import KiwoomWebViewPlus
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.view = KiwoomWebViewPlus()
+        self.splitter = QSplitter(self)
+        self.splitter.setOrientation(Qt.Vertical)
+        layout = QVBoxLayout(self)
+        layout.setMargin(0)
+        layout.addWidget(self.splitter)
+        self.splitter.addWidget(self.view)
+        self.splitter.addWidget(self.view.webInspector)
+
+        html = """
+            <html>
+            <head>
+            </head>
+            <body>
+            <div>
+                <input type="text" id="entry" value="1"/>
+                <input type="button" value="Compute" onclick="updateEntry()"/>
+            </div>
+            <div>
+                <input type="button" value="Quit" onclick="foo.quit()"/>
+            </div>
+            <script>
+            document.addEventListener("kiwoom:connect", function(e) {
+              console.log(e.type, e.detail);
+              kiwoom.setInputValue("종목코드", "000660");
+              console.log(kiwoom.commRqData("Request1", "opt10001", 0, "0101"));
+            });
+            document.addEventListener("kiwoom:receiveTR", function(e) {
+              console.log(e.type, e.detail);
+              console.log(kiwoom.commGetData(e.detail.trCode, "", 0, e.detail.rQName, 0, "종목명"));
+              console.log(kiwoom.commGetData(e.detail.trCode, "", 0, e.detail.rQName, 0, "거래량"));
+            });
+            function updateEntry() {
+                setInterval(function() {
+                    console.log("test...");
+                },1000);
+                kiwoom.commConnect();
+            }
+            </script>
+
+            </body>
+            </html>
+            """
+        self.view.setHtml(html)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    app.exec_()
+
+if __name__ == "__main__":
+    main()
