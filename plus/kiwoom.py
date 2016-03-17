cfb45b3d6a857b6836a48d0c7c14ad28024b8ff8
sculove <sculove@gmail.com>
2016-03-17 23:34:08 +0900

    tr 코드 정리

--- plus/kiwoom.py
+++ plus/kiwoom.py
@@ -22,7 +22,8 @@ class Kiwoom(QObject):
 
 		self.ocx.connect(self.ocx, SIGNAL("OnEventConnect(int)"), self._OnEventConnect)
 		self.ocx.connect(self.ocx, SIGNAL("OnReceiveTrData(QString, QString, QString, QString, QString, int, QString, QString, QString)"), self._OnReceiveTrData)
-		self.ocx.connect(self.ocx, SIGNAL("OnReceiveTrData(QString, QString, QString)"), self._OnReceiveRealData)
+		self.ocx.connect(self.ocx, SIGNAL("OnReceiveMsg(QString, QString, QString, QString)"), self._OnReceiveMsg)
+		self.ocx.connect(self.ocx, SIGNAL("OnReceiveRealData(QString, QString, QString)"), self._OnReceiveRealData)
 
 	@pyqtSlot()
 	def quit(self):
@@ -32,6 +33,15 @@ class Kiwoom(QObject):
 	# 통신 연결 상태 변경시 이벤트
 	def _OnEventConnect(self, errCode):
 		self.view.fireEvent("eventConnect.kiwoom", errCode)
+
+	# 수신 메시지 이벤트
+	def _OnReceiveMsg(self, scrNo, rQName, trCode, msg):
+		self.view.fireEvent("receiveMsg.kiwoom", {
+			"scrNo" : scrNo,
+			"rQName" : rQName,
+			"trCode": trCode,
+			"msg" : msg
+		})
 
 	# Tran 수신시 이벤트
 	def _OnReceiveTrData(self, scrNo, rQName , trCode, recordName, prevNext, dataLength, errorCode, message, splmMsg):
@@ -55,27 +65,54 @@ class Kiwoom(QObject):
 			"realData": realData
 		})
 
-	@pyqtSlot()
+	# 로그인
+	# 0 - 성공, 음수값은 실패
+	@pyqtSlot(result=int)
 	def commConnect(self):
-		self.ocx.dynamicCall("CommConnect()")
+		return self.ocx.dynamicCall("CommConnect()")
 
+	# 로그인 상태 확인
+	# 0:미연결, 1:연결완료, 그외는 에러
+	@pyqtSlot(result=int)
+	def getConnectState(self):
+		return self.ocx.dynamicCall("GetConnectState()")
+
+	# 로그 아웃
 	@pyqtSlot()
 	def commTerminate(self):
 		self.ocx.dynamicCall("CommTerminate()")
 
+	# 로그인한 사용자 정보를 반환한다.
+	# “ACCOUNT_CNT” – 전체 계좌 개수를 반환한다.
+	# "ACCNO" – 전체 계좌를 반환한다. 계좌별 구분은 ‘;’이다.
+	# “USER_ID” - 사용자 ID를 반환한다.
+	# “USER_NAME” – 사용자명을 반환한다.
+	# “KEY_BSECGB” – 키보드보안 해지여부. 0:정상, 1:해지
+	# “FIREW_SECGB” – 방화벽 설정 여부. 0:미설정, 1:설정, 2:해지
+	@pyqtSlot(str,result=str)
+	def getLoginInfo(self, tag):
+		return self.ocx.dynamicCall("GetLoginInfo(QString)",[tag])
+
+
+	# Tran 입력 값을 서버통신 전에 입력값일 저장한다.
 	@pyqtSlot(str, str)
 	def setInputValue(self, id, value):
 		self.ocx.dynamicCall("SetInputValue(QString, QString)", id, value)
 
 	# 통신 데이터를 송신한다.
-# OP_ERR_SISE_OVERFLOW – 과도한 시세조회로 인한 통신불가
-# OP_ERR_RQ_STRUCT_FAIL – 입력 구조체 생성 실패
-# OP_ERR_RQ_STRING_FAIL – 요청전문 작성 실패
-# OP_ERR_NONE – 정상처리
+	# 0이면 정상
+	# OP_ERR_SISE_OVERFLOW – 과도한 시세조회로 인한 통신불가
+	# OP_ERR_RQ_STRUCT_FAIL – 입력 구조체 생성 실패
+	# OP_ERR_RQ_STRING_FAIL – 요청전문 작성 실패
+	# OP_ERR_NONE – 정상처리
 	@pyqtSlot(str, str, int, str, result=int)
 	def commRqData(self, rQName, trCode, prevNext, screenNo):
 		return self.ocx.dynamicCall("CommRqData(QString, QString, int, QString)", rQName, trCode, prevNext, screenNo)
 
+	# 수신 받은 데이터의 반복 개수를 반환한다.
+	@pyqtSlot(str, str, result=int)
+	def getRepeatCnt(self, trCode, recordName):
+		return self.ocx.dynamicCall("GetRepeatCnt(QString, QString)", trCode, recordName)
 
 	# Tran 데이터, 실시간 데이터, 체결잔고 데이터를 반환한다.
 # 1. Tran 데이터
@@ -107,25 +144,11 @@ class Kiwoom(QObject):
 	def getCommRealData(self, realType, fid):
 		return self.ocx.dynamicCall("GetCommRealData(QString, int)",realType, fid)
 
-	# 로그인한 사용자 정보를 반환한다.
-# “ACCOUNT_CNT” – 전체 계좌 개수를 반환한다.
-# "ACCNO" – 전체 계좌를 반환한다. 계좌별 구분은 ‘;’이다.
-# “USER_ID” - 사용자 ID를 반환한다.
-# “USER_NAME” – 사용자명을 반환한다.
-# “KEY_BSECGB” – 키보드보안 해지여부. 0:정상, 1:해지
-# “FIREW_SECGB” – 방화벽 설정 여부. 0:미설정, 1:설정, 2:해지
-	@pyqtSlot(str)
-	def getLoginInfo(self, tag):
-		self.ocx.dynamicCall("GetLoginInfo(QString)",[tag])
-
 	@pyqtSlot(str)
 	def disconnectRealData(self, scnNo):
 		self.ocx.dynamicCall("DisconnectRealData(QString)", scnNo)
 
-	# 수신 받은 데이터의 반복 개수를 반환한다.
-	@pyqtSlot(str, str)
-	def getRepeatCnt(self, trCode, recordName):
-		self.ocx.dynamicCall("GetRepeatCnt(QString, QString)", trCode, recordName)
+
 
 	# 복수종목조회 Tran을 서버로 송신한다.
 # OP_ERR_RQ_STRING – 요청 전문 작성 실패
